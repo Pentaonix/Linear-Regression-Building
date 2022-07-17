154928bb41b6ce1178c01cce74d3263ef938735b
Pentaonix <ducquang0301@gmail.com>
2022-07-18 01:18:05 +0700

    Compare Mean Normalization and Normal Equation in GD

--- predict.py
+++ predict.py
@@ -1,11 +1,18 @@
 import numpy as np  
 from functions import *
 [X, y] = Loadtxt("data.txt")
+#Predict using Mean Normalization
 [X, mu, s] = Normalize(X)
 [Theta, J_hist] = GradientDescent(X,y,0.1,400)
-input = np.array([1,1650])
+input = np.array([19,2790])
 input = (input-mu)/s
 #Lưu ý sửa lại x0 = 1
 input[0] = 1
-predict = predict(input,Theta)
-print("%.2f$"%(predict))
+predict1 = predict(input,Theta)
+print("Predict using Mean Normalization: %.2f$"%(predict1))
+
+#Predict using Normal Equation
+Theta2 = NormEqn(X,y)
+input2 = np.array([19,2790])
+predict2 = predict(input2,Theta2)
+print("Predict using Normal Equation: %.2f$"%(predict2))
